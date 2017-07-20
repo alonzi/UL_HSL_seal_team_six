293076fc169488b5ad69537502ed48186717e71d
pete alonzi <lpa2a@virginia.edu>
2017-07-20 15:59:25 -0400

    make plot of visit v weekday

--- gate_counters/analyse.R
+++ gate_counters/analyse.R
@@ -9,3 +9,10 @@ tib <- tib %>% mutate(dayy=mday(mdy_hm(date)),dai=wday(mdy_hm(date)))
 # sunday = 1
 
 ggplot(data=tib) + geom_point(mapping=aes(x=dayy,y=visitors,color=dai))
+ggplot(tib,aes(dayy,visitors)) + geom_line()
+
+# http://r4ds.had.co.nz/model-building.html#what-affects-the-number-of-daily-flights
+
+tib <- tib %>%
+  mutate(wday=wday(mdy_hm(date),label=TRUE))
+ggplot(tib,aes(wday,visitors)) + geom_boxplot()
