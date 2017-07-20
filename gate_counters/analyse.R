e2b15bac44409bd76a6003c2a5014bb292f0983f
pete alonzi <lpa2a@virginia.edu>
2017-07-20 15:39:33 -0400

    make plot of visits v time

--- gate_counters/analyse.R
+++ gate_counters/analyse.R
@@ -1,1 +1,11 @@
+library(tidyverse)
+library(lubridate)
 
+tib <- read_csv("HSL-2017-07-01-18.csv")
+
+tib <- tib %>% mutate(dayy=mday(mdy_hm(date)),dai=wday(mdy_hm(date)))
+
+# https://rpubs.com/davoodastaraky/lubridate
+# sunday = 1
+
+ggplot(data=tib) + geom_point(mapping=aes(x=dayy,y=visitors,color=dai))
